4f72129142b87175afb56db2ef9a1c602c1d8b73
Oleg Nenashev <o.v.nenashev@gmail.com>
2025-11-27 07:50:18 +0000

    Update the apt index once more before rosdep install

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -4,6 +4,7 @@ set -x
 
 # Initialize and update the ROS environment
 rosdep update
+sudo apt-get update 
 rosdep install --from-paths . --ignore-src -r -i -y --rosdistro kilted
 
 # FIXME: Why does not it install libgz-plugin2-dev libgz-common5-dev libgz-sim8-dev  ?
