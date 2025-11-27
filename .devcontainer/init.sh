5f87d205834f8e791fd4778e32ba6e14d16de922
Oleg Nenashev <o.v.nenashev@gmail.com>
2025-11-27 07:35:13 +0100

    Hardcode ROS distro in the init script
    Signed-off-by: Oleg Nenashev <o.v.nenashev@gmail.com>

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -4,6 +4,6 @@ set -x
 
 # Initialize and update the ROS environment
 rosdep update
-rosdep install --from-paths . --ignore-src -r -i -y --rosdistro ${containerEnv:ROS_VERSION}
+rosdep install --from-paths . --ignore-src -r -i -y --rosdistro kilted
 
 # FIXME: Why does not it install libgz-plugin2-dev libgz-common5-dev libgz-sim8-dev  ?
